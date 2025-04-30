cd15d160cdd36607eafc684ad1a528b5e182e832
davionchai <davionchai@gmail.com>
2025-05-01 00:28:21 +0800

    [Feature] update packer file naming and add timestamp dynamic naming

--- backup_agent/utils/packer.py
+++ backup_agent/utils/packer.py
@@ -6,24 +6,28 @@ from pathlib import Path
 
 logger: logging.Logger = logging.getLogger(__name__)
 
-def create_gzip_file_with_path(source_path: Path, target_path: Path, file_prefix: str) -> Path:
+
+def create_gzip_file_with_path(
+    source_path: Path, target_path: Path, file_prefix: str, file_suffix_timestamp_format: str
+) -> Path:
     """Create a gzip file from the source path and save it to the target path.
 
     Args:
         target_path (Path): The directory where the gzip file will be saved.
         file_prefix (str): The prefix for the gzip file name.
         source_path (Path): The path of the directory to be archived.
+        file_suffix_timestamp_format (str): The format for the timestamp suffix in the gzip file name.
 
     Returns:
         Path: The path of the created gzip file.
     """
-    current_timestamp: str = datetime.now(timezone.utc).strftime("%Y%m%d_%H%M%S")
-    archive_filename: str = f"{file_prefix}_{current_timestamp}.tar.gz"
-    archive_path: Path = Path(f"{target_path}/{archive_filename}")
-    
-    with tarfile.open(archive_path, "w:gz") as tar:
-        tar.add(source_path, arcname=Path(source_path).name)
-    
-    logger.info(f"created backup [{archive_path}]")
+    current_timestamp: str = datetime.now(timezone.utc).strftime(file_suffix_timestamp_format)
+    gzip_filename: str = f"{file_prefix}_{current_timestamp}.tar.gz"
+    gzip_filepath: Path = Path(f"{target_path}/{gzip_filename}")
 
-    return archive_path
+    with tarfile.open(gzip_filepath, "w:gz") as tar:
+        tar.add(source_path, arcname=Path(source_path).name)
+
+    logger.info(f"created backup [{gzip_filepath}]")
+
+    return gzip_filepath
