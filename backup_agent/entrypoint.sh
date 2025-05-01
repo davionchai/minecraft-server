fac1d8a7f5c5714ee4e35792250b7aa44dd65168
davionchai <davionchai@gmail.com>
2025-05-01 13:20:49 +0800

    [Feature] update cron script

--- backup_agent/entrypoint.sh
+++ backup_agent/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "$CRON_SCHEDULE /usr/local/bin/python /app/main.py >> /var/log/cron.log 2>&1" > /etc/cron.d/backup-cron
+echo "$CRON_SCHEDULE python /app/main.py >> /var/log/cron.log 2>&1" > /etc/cron.d/backup-cron
 crontab /etc/cron.d/backup-cron
 cron -f
